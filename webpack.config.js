570a1610431516534bae5ddd57aa6cc1bfb3ce64
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-11-27 01:41:01 +0200

    feat: add pretter config babel config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: "./scripts/app.js",
+  entry: './scripts/app.js',
+  module: {
+    rules: [
+      {
+        test: '/\.js$/',
+      },
+    ],
+  },
 };
