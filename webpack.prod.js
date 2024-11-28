ca00076dd9ea73afec989993ca4c4e548967b585
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-11-28 02:56:39 +0200

    feat: improve optimization and make the website faster by minify the js and css files and assets

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,26 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './scripts/app.js',
   mode: 'production',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true, // Remove console.log statements
+          },
+          output: {
+            comments: false, // Remove comments from output
+          },
+        },
+      }),
+    ],
+  },
   module: {
     rules: [
       {
@@ -12,16 +28,20 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
     ],
   },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
       filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
     }),
   ],
 };
